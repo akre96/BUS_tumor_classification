5b9c37ad94755519e9cde91a41903021e16b4c3b
Samir Akre <sakre@ucdavis.edu>
2020-06-04 18:50:30 -0700

    :construction: testing flask paths

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
+demo_mask = '/data/dataset_busi/normal/normal (100)_mask.png'
+demo_img = '/data/dataset_busi/normal/normal (100).png'
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     image = None
@@ -11,4 +14,5 @@ def home():
             image = url_for('static', filename='demo_image.jpg')
             image_name = 'demo_image'
 
-    return render_template('home.html', image=image, image_name=image_name)
+    return render_template('home.html', image=demo_img, image_name=image_name)
+
