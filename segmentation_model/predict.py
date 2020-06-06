42a9a9a7c2ab6725d8b233390139f2bfa4170332
Anil Yadav <ayadav01@student.coppin.edu>
2020-06-05 18:18:44 -0700

    path_change

--- segmentation_model/predict.py
+++ segmentation_model/predict.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 path_to_weights = "checkpoints/Resnet18_UNet_0.19.pth"
 # USED FOR INFERENCE ONLY
-path_to_image = "C:/Users/AnilYadav/Desktop/Breast/dataset_b/img/000002.png"
+path_to_image = "path_to_image (png/jpg/jpeg)"
 
 if __name__ == "__main__":
     model_obj = Resnet18Unet(path_to_dict=path_to_weights, input_channel=1, num_classes=1, activation='sigmoid')
