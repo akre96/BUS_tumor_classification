be6dc35f286b3ce3d2db91514836f14612502a01
Anil Yadav <ayadav01@student.coppin.edu>
2020-06-05 18:48:00 -0700

    modifying paths

--- segmentation_model/predict.py
+++ segmentation_model/predict.py
@@ -1,16 +1,20 @@
 from model_class import Resnet18Unet
 import matplotlib.pyplot as plt
 
-path_to_weights = "checkpoints/Resnet18_UNet_0.19.pth"
-# USED FOR INFERENCE ONLY
-path_to_image = "path_to_image (png/jpg/jpeg)"
+def get_mask(path_to_image=None, path_to_weights="checkpoints/Resnet18_UNet_0.19.pth"):
+    if path_to_image:
+        model_obj = Resnet18Unet(path_to_dict=path_to_weights, input_channel=1, num_classes=1, activation='sigmoid')
+        prediction = model_obj.get_prediction(path_to_image)
+        return prediction
+    else:
+        print("Image Path Required!")
 
 if __name__ == "__main__":
-    model_obj = Resnet18Unet(path_to_dict=path_to_weights, input_channel=1, num_classes=1, activation='sigmoid')
-    # GET PREDICTION
-    prediction = model_obj.get_prediction(path_to_image)
-    print(type(prediction))
-    print(prediction.shape)
-    plt.imshow(prediction)
-    plt.show()
+    # WEIGHTS FOR MODEL
+    weights_path = "checkpoints/Resnet18_UNet_0.19.pth"
+    # PATH TO IMAGE
+    path_to_image = "path_to_image (png/jpg/jpeg)"
+    # RETURN A NUMPY ARRAY OF SIZE [256, 256]
+    prediction = get_mask(path_to_image, weights_path)
+
 
